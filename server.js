e4d1fd0d556e19f53f634cad28d400c3647613aa
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2024-12-20 20:40:58 -0500

    modified code

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(express.json());
 app.use(bodyParser.json()); // Body parser middleware
 
 
-app.use(cors());
+app.use(cors({
+  origin:["https://vercel.com/kajal-mishras-projects/quiz-client"],
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+}));
 
 // Session configuration
 app.use(session({
