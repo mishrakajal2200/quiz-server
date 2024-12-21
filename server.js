6b335b63f3095b87824c560372b43c5950651eba
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2024-12-21 13:10:09 -0500

    modified code

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const session = require('express-session'); // Import express-session
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -16,6 +17,13 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json()); // Body parser middleware
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.use(cors({
   origin:["https://quiz-client-ruby.vercel.app"],
