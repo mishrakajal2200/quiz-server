640bdae2f73b5321ffb3a71d3e19ea573dcb1313
kajal mishra <111572290+mishrakajal2200@users.noreply.github.com>
2024-12-24 17:24:07 -0500

    netlify url chnages

--- server.js
+++ server.js
@@ -6,25 +6,19 @@ const cors = require('cors');
 const session = require('express-session'); // Import express-session
 const bodyParser = require('body-parser');
 const path = require('path')
-
 // Load environment variables
 dotenv.config();
-
 // Initialize Express app
 const app = express();
-
 // Middleware to parse incoming JSON and form data
 app.use(express.json());
 app.use(bodyParser.json()); // Body parser middleware
 
-
-
 app.use(cors({
-  "origin":["https://heartfelt-torrone-49dd5e.netlify.app"],
+  "origin":["https://clever-arithmetic-77cc2f.netlify.app"],
   "methods":["GET","POST","PUT","DELETE"],
   "credentials":true
 }));
-
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
@@ -67,9 +61,9 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Error connecting to MongoDB:', err));
-
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
